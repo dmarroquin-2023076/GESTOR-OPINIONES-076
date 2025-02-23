5ebd0458e4fbfe6ca5f8e2d5f874e969ce3065e1
dmarroquin2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-02-22 20:28:12 -0600

    Se agregan funciuones a la categoria.

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -22,6 +22,91 @@ export const saveCategory = async (req, res)=>{
     }
 }
 
+export const update = async (req, res)=>{
+  try {
+    let id = req.params.id
+    let data = req.body
+
+    let updateCategory = await Category.findByIdAndUpdate(
+      id, 
+      data,
+      { new: true }
+    )
+    if (!updateCategory){
+      return res.status(400).send(
+            {
+                success: false,
+                 message: 'Category not found and not updated'
+            }
+      )
+    }
+    return res.send(
+      {
+       success: true,
+       message: 'Category  updated',
+       user: updateCategory 
+       }
+    )
+ } catch (err) {
+    console.error('General error', err)
+    return res.status(500).send({
+        success: false,
+        message: 'General error',
+        err
+    })
+}
+}
+
+// export const categoryDelete = async (req, res) => {
+//     try {
+//         let { id } = req.params
+
+//         const categoryToDelete = await Category.findById(id)
+//         if (!categoryToDelete) {
+//             return res.status(404).send({
+//                 success: false,
+//                 message: 'Category not found'
+//             })
+//         }
+
+        
+//         const defaultCategory = await Category.findOne({ name: 'Default' })
+//         if (!defaultCategory) {
+//             return res.status(500).send({
+//                 success: false,
+//                 message: 'Default category not found'
+//             })
+//         }
+
+       
+//         await Public.updateMany(
+//             { category: id }, 
+//             { category: defaultCategory._id } 
+//         )
+
+//         // Eliminar la categoría
+//         let deleteCategory = await Category.findByIdAndDelete(id);
+        
+//         return res.status(200).send({
+//             success: true,
+//             message: 'Category deleted successfully',
+//             deleteCategory
+//         })
+//     } catch (e) {
+//         console.error(e);
+//         return res.status(500).send({
+//             success: false,
+//             message: 'General error',
+//              error: e
+//          })
+//      }
+//  }
+
+
+
+
+
+
 
 
 
--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { saveCategory } from "./category.controller.js"
-
+import { saveCategory, update } from "./category.controller.js"
+import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
 const api = Router()
 
 
 
-api.post('/new', saveCategory)
+api.post('/new', validateJwt, isAdmin, saveCategory)
+api.put('/update/:id',validateJwt, isAdmin, update)
 
 
 export default api
